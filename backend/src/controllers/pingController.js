4aa45bb6ca36cbe1554f1df987d0d2996cffd657
ramesh-kunwar <rameshkunwar1110@gmail.com>
2025-01-04 21:14:57 +0545

    Folder Clean up

--- backend/src/controllers/pingController.js
+++
@@ -1,5 +0,0 @@
-export async function pingCheck(req, res) {
-  return res.status(200).json({
-    msg: "Ping Check",
-  });
-}
--- backend/src/routes/v1/projects.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createProjectController } from "../../controllers/projectControllr.js";
-
-const router = express.Router();
-
-router.post("/", createProjectController);
-
-export default router;
